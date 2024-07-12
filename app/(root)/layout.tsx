23c6c5e79526f50f84455c0e0fe79e48cdf0d200
Ifeani-Ani <ifeanyiani155@gmail.com>
2024-07-12 18:59:45 +0100

    feat: add group root route

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 
