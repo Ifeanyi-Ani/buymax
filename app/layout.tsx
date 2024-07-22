fefbfde220a3c82bcfb3848bbdb0665d5011186c
Ifeani-Ani <ifeanyiani155@gmail.com>
2024-07-22 18:10:28 +0100

    feat: add themeprovider from shadcn

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+import "@/app/globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+      <html lang="en">
+        <body
+        >
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
