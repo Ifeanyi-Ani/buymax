eb6b1c4adf9af0f928fc80e49a4e40b419b6743a
Ifeani-Ani <ifeanyiani155@gmail.com>
2024-07-22 18:11:27 +0100

    style: add custom font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
+import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
 import "@/app/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+const fontSans = FontSans({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
