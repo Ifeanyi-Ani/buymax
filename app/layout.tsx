7c828dffa216d2c43b121c9a07add99dec5909fa
Ifeani-Ani <ifeanyiani155@gmail.com>
2024-07-22 18:12:44 +0100

    style: loyout body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
 import "@/app/globals.css";
+import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -21,6 +22,10 @@ export default function RootLayout({
   return (
       <html lang="en">
         <body
+          className={cn(
+            "min-h-screen dark:bg-primary-950 font-sans antialiased",
+            fontSans.variable,
+          )}
         >
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
