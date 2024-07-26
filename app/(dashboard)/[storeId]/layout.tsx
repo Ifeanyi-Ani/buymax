20234924e7bb4390f388826e432ce4e9a1f436ee
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-07-26 22:30:01 +0100

    feat: get the loggedin userId from clerk and verify
    see: gh-11

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -10,4 +10,10 @@ export default async function DashboardLayout({
   children: React.ReactNode;
   params: { storeId: string };
 }) {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
 }
