5f4a64ddaa9103dae6102c3fe010b3c6e2f8492d
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-07-26 21:04:14 +0100

    feat: destructure the name field from our req.
    see: gh-7

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -4,6 +4,9 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
     const { userId } = auth();
+    const body = await req.json();
+
+    const { name } = body;
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
