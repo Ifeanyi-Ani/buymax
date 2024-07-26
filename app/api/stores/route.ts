f6484c769d948e54b78f1fb2d13054c665c2acd9
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-07-26 21:01:30 +0100

    feat: destructure our loggedin user from clerk
    see: gh-7

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -2,3 +2,6 @@ import prismaDb from "@/lib/prismaDb";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+export async function POST(req: Request) {
+    const { userId } = auth();
+}
