45245c1fef82aec19746714de539e5662f9dba76
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-07-26 21:03:15 +0100

    feat: handle no loggedin user
    if there is no logged in user return unauthorized
    see: gh-7

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -4,4 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
     const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
 }
