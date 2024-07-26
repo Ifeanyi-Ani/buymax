68c955120f6c1db2126edc3794d22b593b436622
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-07-26 22:27:32 +0100

    feat: import necessary modules
    see: gh-11

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,8 @@
+import prismaDb from "@/lib/prismaDb";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
+}) {
+}
