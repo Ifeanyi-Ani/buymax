05a72e6d102e682c8edae8580b6dabfaf8ceb40f
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-07-26 22:28:47 +0100

    chore: destructure the children and params

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -4,5 +4,10 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { storeId: string };
 }) {
 }
