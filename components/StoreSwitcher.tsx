a97581eeceda13b54c96310e8492bd424677021b
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-01 22:00:23 +0100

    chores: dependecies needed for the storeswitch component
    see: gh-14

---
+++ components/StoreSwitcher.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { Store } from "@prisma/client";
+import { useParams, useRouter } from "next/navigation";
+import { useStoreModal } from "@/hooks/use-store-model";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import {
+  Check,
+  ChevronsUpDown,
+  PlusCircle,
+  Store as StoreIcon,
+} from "lucide-react";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "@/components/ui/command";
+
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<
+  typeof PopoverTrigger
+>;
+interface StoreSwitcherProps extends PopoverTriggerProps {
+  items: Store[];
+}
+export default function StoreSwticher({
+  className,
+  items = [],
+}: StoreSwitcherProps) {
+  return (
+  );
+}
