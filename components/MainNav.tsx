f97c326f910b1d8955730735e9d00bb049e8735e
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-01 22:16:20 +0100

    feat: creat array of links info
    see: gh-15

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -9,6 +9,14 @@ type MainNavProps = {} & React.HTMLAttributes<HTMLElement>;
 export default function MainNav({ className, ...props }: MainNavProps) {
   const pathname = usePathname();
   const { storeId } = useParams();
+
+  const routes = [
+    {
+      href: `/${storeId}/settings`,
+      label: "Settings",
+      active: pathname === `/${storeId}/settings`,
+    },
+  ];
   return (
     <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
     </nav>
