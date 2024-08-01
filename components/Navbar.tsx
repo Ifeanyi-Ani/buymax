88125c59c18de77601711307aaf4e7779c1c5bb6
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-01 21:47:59 +0100

    feat: navbar component
    see: gh-13

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,3 +5,12 @@ import StoreSwticher from "./StoreSwitcher";
 import prismaDb from "@/lib/prismaDb";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+const Navbar = async () => {
+  return (
+    <div className="border-b ">
+      <div className="flex h-16 items-center px-4 gap-4">
+      </div>
+    </div>
+  );
+};
+export default Navbar;
