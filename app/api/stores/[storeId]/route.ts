bce2afd22e08ec502934b9b83bce8f5d6ab1d619
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-09 10:16:50 +0100

    feat: get data and check for their existence
    see: gh-24

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -2,3 +2,25 @@ import prismaDb from "@/lib/prismaDb";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string } },
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { name } = body;
+    const { storeId } = params;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("name is required", { status: 400 });
+    }
+
+    if (!storeId) {
+      return new NextResponse("storeId is required", { status: 400 });
+    }
+}
