79a7b60e271bfb0ac8d08551fb50eb989582a756
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-09 10:34:49 +0100

    chore: import dependencies
    see: gh-27

---
+++ app/(dashboard)/[storeId]/(routes)/settings/_components/SettingForm.tsx
@@ -0,0 +1,22 @@
+"use client";
+import * as z from "zod";
+import axios from "axios";
+import { Store } from "@prisma/client";
+import React, { useState } from "react";
+import { Heading } from "./Heading";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
+
