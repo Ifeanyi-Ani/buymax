07139ba880680992d6be2bd63c113e9b44442c7b
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-20 11:28:20 +0100

    chores: dependencies

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/_components/BillboardClient.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/Heading";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { Separator } from "@/components/ui/separator";
+
