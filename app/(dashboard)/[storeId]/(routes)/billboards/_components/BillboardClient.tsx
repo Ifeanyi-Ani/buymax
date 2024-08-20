140614e35c5673bc5d5740cf7d1f8fe65c220f12
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-20 11:29:52 +0100

    feat: billboard overview component

--- app/(dashboard)/[storeId]/(routes)/billboards/_components/BillboardClient.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/_components/BillboardClient.tsx
@@ -6,3 +6,24 @@ import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { Separator } from "@/components/ui/separator";
 
+export const BillboardClient = () => {
+  const router = useRouter();
+  const { storeId } = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title="Billboard(0)"
+          description="Manage billboard for your store"
+        />
+
+        <Button onClick={() => router.push(`/${storeId}/billboards/new`)}>
+          <Plus className="h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+
+      <Separator />
+    </>
+  );
+};
