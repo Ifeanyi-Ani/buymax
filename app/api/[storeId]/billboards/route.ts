1636935f0932adc6578628e0df754928a8dc7b8e
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-20 12:11:47 +0100

    feat: get body for req and check if variables exist

--- app/api/[storeId]/billboards/route.ts
+++ app/api/[storeId]/billboards/route.ts
@@ -14,6 +14,26 @@ export async function POST(req: Request, props: PostProps) {
     // Extract the authenticated user's ID from the request context, in my case clerk
     const { userId } = auth();
 
+    // Parse request body as JSON and extract store details
+    const body = await req.json();
+    const { label, imageUrl } = body;
+
+    // Validate user ID.
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    // Validate store ID and store details before creating a new store in the database.
+    if (!storeId) {
+      return new NextResponse("storeId is required", { status: 400 });
+    }
+
+    if (!label && !imageUrl) {
+      return new NextResponse("Label and image URL are required", {
+        status: 400,
+      });
+    }
+
   } catch (error) {
     console.error("BILLBOARD_POST", error);
     return new NextResponse("Internal Server Error", { status: 500 });
