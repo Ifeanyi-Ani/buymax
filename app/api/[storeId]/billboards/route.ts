b02b8a34464a766f3e4585b8397cdb56c07e8d0c
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-20 12:09:42 +0100

    feat: extract storeId from params

--- app/api/[storeId]/billboards/route.ts
+++ app/api/[storeId]/billboards/route.ts
@@ -5,6 +5,11 @@ interface PostProps {
 }
 export async function POST(req: Request, props: PostProps) {
   try {
+    // Extract store ID from the request parameters
+    const {
+      params: { storeId },
+    } = props;
+
   } catch (error) {
     console.error("BILLBOARD_POST", error);
     return new NextResponse("Internal Server Error", { status: 500 });
