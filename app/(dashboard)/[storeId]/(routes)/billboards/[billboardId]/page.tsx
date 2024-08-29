17e16b1a8cf7d2efd0766419e2dc67eb1acea188
Ifeanyi Ani <ifeanyiani155@gmail.com>
2024-08-29 15:23:55 +0100

    change tag to section

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -13,11 +13,11 @@ const BillboardPage = async ({
     },
   });
   return (
-    <div className="flex flex-col">
+    <section className="flex flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
         <BillboardForm initialData={billboard} />
       </div>
-    </div>
+    </section>
   );
 };
 
